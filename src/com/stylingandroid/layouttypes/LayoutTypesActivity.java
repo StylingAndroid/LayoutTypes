4883a6e5b76487ab69256d9b90a78cd81c1299cd
Mark Allison <mark@stylingandroid.com>
2011-08-05 10:21:59 +0100

    Completed LinearLayout code.

--- src/com/stylingandroid/layouttypes/LayoutTypesActivity.java
+++ src/com/stylingandroid/layouttypes/LayoutTypesActivity.java
@@ -3,11 +3,13 @@ package com.stylingandroid.layouttypes;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class LayoutTypesActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class LayoutTypesActivity extends Activity
+{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.linear );
+	}
 }
